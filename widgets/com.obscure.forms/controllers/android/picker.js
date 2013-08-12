28bd0c308e7c5a3a7066108d0b2a8474ea6ae82e
Paul Mietz Egli <paul@obscure.com>
2013-08-12 08:34:41 -0700

    created platform-specific controllers for picker widget

---
+++ widgets/com.obscure.forms/controllers/android/picker.js
@@ -0,0 +1,55 @@
+var $model;
+var args = arguments[0] || {};
+var containerHeight;
+
+var collection = Alloy.Collections[args.sourceCollection];
+if (!collection) {
+  throw("Missing source collection '" + args.sourceCollection + "'");
+}
+
+collection.on('fetch', function() {
+  var selectedRow = undefined;
+  var val = $model && args.field ? $model.get(args.field) : undefined;
+  
+  var opts = collection.map(function(item) {
+    return item.get(args.sourceField);
+  });
+
+  var dialog = Ti.UI.createOptionDialog({
+    options: opts,
+    selectedIndex: _.indexOf(opts, val)
+  });
+  
+  dialog.addEventListener('click', function(e) {
+    if (!e.cancel && $model && args.field) {
+      this.hide();
+      $model.set(args.field, opts[e.index]);
+      refresh();
+    }
+  });
+  
+  dialog.show();
+});
+
+// set the label text to the localized value of the "label" argument 
+if (args.label) {
+  $.form_label.text = L(args.label, args.label);  
+}
+
+function refresh() {
+  $.form_picker_label.text = $model && args.field ? $model.get(args.field) : '';
+}
+
+// exports
+
+exports.bindModel = function(model) {
+  $model = model;
+  refresh();
+};
+
+
+// events
+
+function togglePicker(e) {
+  collection.fetch();
+}
